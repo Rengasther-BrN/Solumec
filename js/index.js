87893f353f7417d798eda85df15f0dd0e973369b
Bruno Marques <brunoaffonsomarques@gmail.com>
2022-07-27 11:56:23 -0300

    Version 1.5
    send e-mail

---
+++ js/index.js
@@ -0,0 +1,16 @@
+﻿function Click() {
+var link = "";
+
+var name = document.querySelector("#txtName").value;
+var company = document.querySelector("txtCompany").value;
+var email = document.querySelector("txtEmail").value;
+var phone = document.querySelector("txtPhone").value;
+var product = document.querySelector("txtProduct").value;
+
+    link = `name: ${name}`+`testetete` + `fsdfsdfsdfsdf`;
+    alert("link"); 
+    
+        window.location.replace('https://api.whatsapp.com/send?phone=55193631010&text=Ol%C3%A1%2C%20tenho%20interesse%20em%20um%20ve%C3%ADculo%5Cservi%C3%A7o%20Mega%20Autos.%20Favor%20entrar%20em%20contato%20Nome:%20');
+
+}
+
